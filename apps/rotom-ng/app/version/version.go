118d562d414f0b84815d2dd2c535b684781b302a
Chris Behrens <cbehrens@codestud.com>
2026-08-15 15:32:18 +0000

    Bump version to 1.0.0

--- apps/rotom-ng/app/version/version.go
+++ apps/rotom-ng/app/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // This file is generated from version.txt. Do not edit directly.
 // AppVersion is the current application version.
-const AppVersion = "1.0.0alpha6"
+const AppVersion = "1.0.0"
